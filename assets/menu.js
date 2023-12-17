67661bffbe835f82cf509ae369e0f67567f51fd1
Stofeles412 <fernandostofeles@gmail.com>
2023-12-16 22:34:12 -0300

    atualizei

--- assets/menu.js
+++ assets/menu.js
@@ -1,5 +1,5 @@
 function menuShow(){
-    var menuMobile = window.document.getElementsByClassName('.mobile-menu')
+    var menuMobile = window.document.querySelector('.mobile-menu')
     if(menuMobile.classList.contains('open')){
     menuMobile.classList.remove('open')
 }else{
