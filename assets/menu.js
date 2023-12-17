08b1b3a68d309ae27b764fd6b8182825d2c2383c
Stofeles412 <fernandostofeles@gmail.com>
2023-12-17 08:33:32 -0300

    .

--- assets/menu.js
+++ assets/menu.js
@@ -1,8 +1,9 @@
 function menuShow(){
     var menuMobile = window.document.querySelector('.mobile-menu')
-    if(menuMobile.classList.contains('open')){
+    if (menuMobile.classList.contains('open')){
     menuMobile.classList.remove('open')
 }else{
     menuMobile.classList.add('open')
+   
 }
 }
