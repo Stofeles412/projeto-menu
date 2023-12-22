54e5afdd3e04f9029d65d9c788b821b750f9b6ab
Stofeles412 <fernandostofeles@gmail.com>
2023-12-22 13:40:35 -0300

    estudando

--- assets/menu.js
+++ assets/menu.js
@@ -1,5 +1,5 @@
 function menuShow() {
    var menu = window.document.getElementById('itens')
-   var ul = window.document.getElementById('mobile-menu')
+   var ul = window.document.getElementById('#mobile-menu')
    menu.style.display = 'block'
 }
