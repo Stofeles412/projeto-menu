0839153ef8b33c497be50e5c9939a5dc0f6c66b2
Stofeles412 <fernandostofeles@gmail.com>
2023-12-22 14:45:49 -0300

    estudei, com um pouco de progresso

--- assets/menu.js
+++ assets/menu.js
@@ -1,5 +1,7 @@
 function menuShow() {
    var menu = window.document.getElementById('itens')
-   var ul = window.document.getElementById('#mobile-menu')
-   menu.style.display = 'block'
+   var ul = window.document.getElementById('open')
+   var abrir = window.document.getElementById('openmenu')
+   var botao = window.document.getElementById('menu-icon')
+    ul.abrir.style.display ='block'
 }
