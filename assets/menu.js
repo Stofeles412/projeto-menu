fbadbd5a453f334f195fc6988eb39c7b28418cda
Stofeles412 <fernandostofeles@gmail.com>
2023-12-26 14:09:10 -0300

    .

--- assets/menu.js
+++ assets/menu.js
@@ -2,5 +2,5 @@ function menuShow() {
     var menu = window.document.getElementById ('abrir')
     var botao = window.document.getElementById ('menu-icon')
     var abrir = window.document.getElementById('open')
-    abrir.botao.style.display = 'block'
+    botao.style.display = 'block'
  }
