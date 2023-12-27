79d56a289d309fd2d5917b98151857ddf680f859
Stofeles412 <fernandostofeles@gmail.com>
2023-12-27 16:10:40 -0300

    alterei por completo

--- assets/menu.js
+++
@@ -1,7 +0,0 @@
-function menuShow() {
-    var menu = window.document.getElementById('open')
-    var menu = window.document.getElementById ('abrir')
-    var botao = window.document.getElementById ('menu-icon')
-   
-    menu.style.display = 'block'
- }
