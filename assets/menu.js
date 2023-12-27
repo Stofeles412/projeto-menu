8cb3ba1858b8581f35b532982cf596709927173a
Stofeles412 <fernandostofeles@gmail.com>
2023-12-27 00:08:36 -0300

    estudei bem

--- assets/menu.js
+++ assets/menu.js
@@ -1,6 +1,7 @@
 function menuShow() {
+    var menu = window.document.getElementById('open')
     var menu = window.document.getElementById ('abrir')
     var botao = window.document.getElementById ('menu-icon')
-    var abrir = window.document.getElementById('menu-aberto')
-    abrir.menu.style.display =  'block'
+   
+    menu.style.display = 'block'
  }
